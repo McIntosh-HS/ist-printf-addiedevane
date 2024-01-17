8ffe78f00e1db741d334c82e8ff6ca5347ab1b4d
Addie DeVane <aadevane123@gmail.com>
2024-01-17 13:40:26 -0500

    added

--- integers.c
+++ integers.c
@@ -3,11 +3,11 @@
 int main()
 {
 	int a = 10;
-	// printf("a displayed as base 10 integer: %3\n", a);
-	// printf("a displayed as base 8 integer: %7\n", a);
-	// printf("a displayed as base 16 integer: %10\n", a);
-	// printf("a displayed as base 16 integer: %19\n", a);
-	// printf("a displayed as base 10 unsigned integer: %7\n", a);
+	// printf("a displayed as base 10 integer: %i\n", a);
+	// printf("a displayed as base 8 integer: %o\n", a);
+	// printf("a displayed as base 16 integer: %x\n", a);
+	// printf("a displayed as base 16 integer: %X\n", a);
+	// printf("a displayed as base 10 unsigned integer: %u\n", a);
 
 	return 0;
 }
