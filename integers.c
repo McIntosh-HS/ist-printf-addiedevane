281ad96d7bd3fae8472e450af5d9eb009c5503e5
Addie DeVane <aadevane123@gmail.com>
2024-01-17 13:33:45 -0500

    a

--- integers.c
+++ integers.c
@@ -7,7 +7,7 @@ int main()
 	char c = '2';
 
 	// Format specifiers are placeholders for variable data	printf
-	// printf("a displayed as base 10 integer: %i\n", a); 
+	// printf("a displayed as base 10 integer: %i\n", a);
 	// printf("a displayed as base 8 integer: %o\n", a);
 	// printf("a displayed as base 16 integer: %x\n", a);
 	// printf("a displayed as base 16 integer: %X\n", a);
