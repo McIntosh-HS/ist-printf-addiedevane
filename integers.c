19edddef921d227f06ac4487117d9d0d3460e505
Addie DeVane <aadevane123@gmail.com>
2024-01-17 13:54:56 -0500

    add

--- integers.c
+++ integers.c
@@ -3,11 +3,13 @@
 int main()
 {
 	int a = 10;
-	// printf("a displayed as base 10 integer: %i\n", a);
-	// printf("a displayed as base 8 integer: %o\n", a);
-	// printf("a displayed as base 16 integer: %x\n", a);
-	// printf("a displayed as base 16 integer: %X\n", a);
-	// printf("a displayed as base 10 unsigned integer: %u\n", a);
+
+	// Format specifiers are placeholders for vaiable data
+	printf("a displayed as base 10 integer: %i\n", a);     // %i is for integers
+	printf("a displayed as base 8 integer: %o\n", a);     // %i is for integers
+	printf("a displayed as base 16 integer: %x\n", a);     // %i is for integers
+	printf("a displayed as base 16 integer: %X\n", a);     // %i is for integers
+	printf("a displayed as base 10 unsigned integer: %u\n", a);     // %i is for integers
 
 	return 0;
 }
